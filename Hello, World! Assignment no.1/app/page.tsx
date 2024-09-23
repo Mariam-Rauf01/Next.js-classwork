4f9214dd6bc9e1115e19fdee4eaa2d7a1dbda23a
Mariam Rauf <160196063+Mariam-Rauf01@users.noreply.github.com>
2024-09-22 17:55:57 -0700

    My first Next.js project.
    🚀 Completed Assignment 1: Hello World in Next.js
    
    I have successfully completed my first assignment in Next.js, where I developed a simple "Hello World" application utilizing Tailwind CSS for styling. This project allowed me to familiarize myself with the fundamentals of server-side rendering and static site generation, while also enhancing my skills in responsive design with Tailwind. I am excited to explore more advanced features and continue building dynamic web applications.

---
+++ Hello, World! Assignment no.1/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <div className="bg-white p-10 rounded-lg shadow-lg">
+      <h1 className="text-slate-700 text-4xl font-bold">Hello, World!</h1>
+      </div>
+    </div>
+  );
+}
